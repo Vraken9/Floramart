70fd4ad3a68e04301d8c534b1bc0d55f67337aed
Vraken9 <keteijo123@gmail.com>
2026-05-16 20:47:43 +0700

    feat(database): create shops and categories schemas with relational integrity

---
+++ database/migrations/2026_05_16_134424_create_shops_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+  public function up(): void
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('district_id')->constrained('districts');  
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('address_detail'); 
+            $table->string('whatsapp_number'); 
+            $table->enum('status', ['pending', 'approved', 'suspended'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shops');
+    }
+};
