2656570910bfb3428478213a8db1be1fe5c9b50d
Vraken9 <keteijo123@gmail.com>
2026-05-24 08:53:38 +0700

    fix: resolve 403 invalid signature by trusting proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => RoleMiddleware::class,
         ]);
+        
+        // Trust all proxies for Cloud/Docker deployment (solves 403 Invalid Signature)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
