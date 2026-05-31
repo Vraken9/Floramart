987248fea1e8cc01d3422c76ab201bea1396ce81
Vraken9 <keteijo123@gmail.com>
2026-05-31 18:34:21 +0700

    Fix: Increase maxOutputTokens for Gemini API to prevent AI response truncation

--- app/Http/Controllers/ChatbotController.php
+++ app/Http/Controllers/ChatbotController.php
@@ -70,7 +70,7 @@ Pertanyaan pengguna: \"$userMessage\"";
                     'temperature' => 0.7,
                     'topK' => 40,
                     'topP' => 0.95,
-                    'maxOutputTokens' => 800,
+                    'maxOutputTokens' => 2500,
                 ]
             ]);
 
